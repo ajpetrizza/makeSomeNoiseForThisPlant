631cad871c228ff9b611d3982d662f74a1acfe93
ajpetrizza <ajpetrizza@fullsail.edu>
2021-06-30 23:44:06 -0400

    First message to shay

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello guys
+          WE did it shay
         </a>
       </header>
     </div>
